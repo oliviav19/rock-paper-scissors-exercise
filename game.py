632d0a50ddc29aa582be2576b60a41d32dc8638c
Olivia Vitali <orv208@stern.nyu.edu>
2021-05-25 21:33:15 -0400

    attempt at determining winner

--- game.py
+++ game.py
@@ -24,7 +24,8 @@ valid_options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(valid_options)
 print("COMPUTER CHOICE: ", computer_choice)
 
-
+if(if(user_choice == "rock") and (computer_choice =="rock")) or ((user_choice == "paper") and (computer_choice == "paper")) or ((user_choice == "scissors") and (computer_choice == "scissors"))
+print("TIE")
 
 print("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN.")
 
