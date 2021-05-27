08f5ba6165ab5b9323e080484f48f6c859ff5164
Olivia Vitali <orv208@stern.nyu.edu>
2021-05-27 18:25:09 -0400

    Determine Winner

--- game.py
+++ game.py
@@ -24,8 +24,26 @@ valid_options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(valid_options)
 print("COMPUTER CHOICE: ", computer_choice)
 
-if(if(user_choice == "rock") and (computer_choice =="rock")) or ((user_choice == "paper") and (computer_choice == "paper")) or ((user_choice == "scissors") and (computer_choice == "scissors"))
-print("TIE")
+#USER WINNING AND TIE SCENARIOS
+
+if user_choice == computer_choice:
+    print("It's a tie, try again!")
+elif (user_choice == "scissors") and (computer_choice == "paper"):
+    print("Scissors cuts paper, you win!")
+elif (user_choice == "rock") and (computer_choice == "scissors"):
+    print("rock smashes scissors, you win!")
+elif (user_choice == "paper") and (computer_choice == "rock"):
+    print("Paper covers rock, you win!")
+
+#COMPUTER WINNING SCENARIOS
+
+elif (user_choice == "scissors") and (computer_choice == "rock"):
+    print("rock smashes scissors, you lose!")
+elif (user_choice == "rock") and (computer_choice == "paper"):
+    print("paper covers rock, you lose!")
+elif (user_choice == "paper") and (computer_choice == "scissors"):
+    print("Scissors cuts paper, you lose!")
+
 
 print("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN.")
 
